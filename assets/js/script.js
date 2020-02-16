9382b2b14faa01d1e5b4d842645097d4a6ab0342
8rain1ag <asleeppiano@outlook.com>
2020-02-17 00:26:03 +0300

    index page done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,29 @@
-const navButton = document.querySelector('.header__menu-btn')
-const headerNavMobile = document.querySelector('.header-nav.mobile')
-const navCloseButton = document.querySelector('.header-nav__close-btn')
+const navButton = document.querySelector('.mb-header__menu-btn')
+const headerNavMobile = document.querySelector('.mb-header-nav')
+const navCloseButton = document.querySelector('.mb-header-nav__close-btn')
+const headerNavListItems = document.querySelectorAll('.mb-header-nav-list__item')
 
 navButton.addEventListener('click', () => {
-    headerNavMobile.setAttribute('active', '')
+  headerNavMobile.style.opacity = '0'
+  headerNavMobile.classList.add('active')
+  window.requestAnimationFrame(() => {
+    headerNavMobile.style.opacity = '1'
+    headerNavListItems.forEach((item, i) => {
+      console.log(item)
+      setTimeout(() => {
+        item.style.opacity = 1;
+      }, 90 * i)
+    })
+  })
     function closeHeaderNavHandler() {
-        headerNavMobile.removeAttribute('active')
+        headerNavMobile.style.opacity = '0'
+      setTimeout(() => {
+        headerNavListItems.forEach(item => {
+          item.style.opacity = 0
+        })
+        headerNavMobile.classList.remove('active')
         navCloseButton.removeEventListener('click', closeHeaderNavHandler)
+      }, 300)
     }
     navCloseButton.addEventListener('click', closeHeaderNavHandler)
 })
